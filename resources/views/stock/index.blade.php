023bdd948d0e9fb7bbba417e985a5b8309cc94b6
AguBaste <agustinbasterrica@hotmail.com>
2024-07-14 16:53:10 -0300

    actualizando

--- resources/views/stock/index.blade.php
+++ resources/views/stock/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <h1 class="titulo">stock</h1>
-    <img class="imagen" src="{{asset('img/stock.jpeg')}}" alt="">
+    <img class="imagen" src="{{ asset('img/stock.jpeg') }}" alt="">
     @if ($stocks->isEmpty())
         <h1>no tienes productos en stock</h1>
     @else
@@ -13,20 +13,22 @@
                 <tr>
                     <td></td>
                     <th>descripcion</th>
-                    <th>quedan</th>
+                    <th>bolsas</th>
+                    <th>kilos</th>
                 </tr>
             </thead>
             <tbody>
 
                 @foreach ($stocks as $stock)
                     <tr>
-                        <td><img src="{{asset('upload/'.$stock->image)}}" class="mini-imagen" alt=""></td>
-                        <td>{{ $stock->product->brand->desc . ' ' . $stock->product->race->desc . ' ' . $stock->product->flavor->desc . ' ' . $stock->product->weight . ' kg' }}   </td>
-                        {{-- hacer un if y modificar los span sacarlos del table y poner
-                        .span-verde o span-rojo --}}
-                        <td><span
-                                class="{{ $stock->quantity <= $stock->product->weight / 2 ? 'texto-rojo' : 'texto-verde' }}">{{ $stock->quantity }}
-                                </span>kilos</td>
+                        <td><img src="{{ asset('upload/' . $stock->image) }}" class="mini-imagen" alt=""></td>
+                        <td>{{ $stock->product->brand->desc . ' ' . $stock->product->race->desc . ' ' . $stock->product->flavor->desc . ' ' . $stock->product->weight . ' kg' }}
+                        </td>
+                        <td>
+                            {{ floor($stock->quantity / $stock->product->weight) }}</td>
+                        <td>
+                            {{ ($stock->quantity / $stock->product->weight - floor($stock->quantity / $stock->product->weight)) * $stock->product->weight }} kg
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
